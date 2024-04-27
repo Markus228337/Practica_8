e30e62130a4bc13c5e386567b08d3ffd3167fba2
Markus228337 <133499221+Markus228337@users.noreply.github.com>
2024-04-27 17:35:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication99/DataSources/Room/DAO/TaskDao.java
@@ -0,0 +1,23 @@
+package com.example.myapplication99.DataSources.Room.DAO;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.mobiles4.Data.DataSources.Room.Entities.Task;
+
+import java.util.List;
+
+@Dao
+public interface TaskDao {
+    @Query("SELECT * FROM Task")
+    public List<Task> getTasks();
+    @Query("SELECT * FROM Task WHERE id == (:index)")
+    public Task get(int index);
+    @Insert
+    public void add(Task task);
+    @Query("DELETE FROM Task")
+    public void clearAll();
+    @Query("SELECT COUNT(*) FROM Task")
+    public int getSize();
+}
